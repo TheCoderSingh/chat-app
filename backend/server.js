55231e5f88a21536c170b2ac7e7a2023724baeca
Jaskaran Singh <thecodersingh@gmail.com>
2024-03-06 01:55:58 -0800

    Implement socket.io, complete implementation

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,10 @@ import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import cookieParser from "cookie-parser";
 
+import { app, server } from "./socket/socket.js";
+
 dotenv.config();
 
-const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json()); // Parse incoming requests data (from req.body)
@@ -20,7 +21,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server running on port ${PORT}`);
 });
